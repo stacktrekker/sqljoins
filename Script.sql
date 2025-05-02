311ee7ce8011c7af0fc242e36599c3b5894c6a00
stacktrekker <stacktrekker@gmail.com>
2025-05-02 18:12:15 +0200

    Delete Script.sql

--- Script.sql
+++
@@ -1,9 +0,0 @@
-SELECT e.emp_id, e.name AS employee_name, d.name AS department_name
-FROM EMPLOYEES e 
-INNER JOIN DEPARTMENTS d ON e.DEPT_ID = d.DEPT_ID
-
-UNION
-
-SELECT e.emp_id, e.name AS employee_name, d.name AS department_name
-FROM EMPLOYEES e 
-RIGHT JOIN DEPARTMENTS d ON e.DEPT_ID = d.DEPT_ID;
